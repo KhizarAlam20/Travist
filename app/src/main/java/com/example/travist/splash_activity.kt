ca2476b2f61960becf461a1b5f7310848d9e3072
KhizarAlam20 <khizaralam20@gmail.com>
2023-10-04 01:00:21 -0700

    Initial commit

---
+++ app/src/main/java/com/example/travist/splash_activity.kt
@@ -0,0 +1,20 @@
+package com.example.travist
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class splash_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        val intent = Intent (this,MainActivity::class.java)
+
+        Handler().postDelayed({
+            startActivity(intent)
+            finish()
+        },2000)
+
+    }
+}
