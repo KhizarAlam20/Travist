5127ab3ac9ed64a7d342792c13958457587ea774
KhizarAlam20 <khizaralam20@gmail.com>
2023-10-05 01:17:10 -0700

    updated login screen

--- app/src/main/java/com/example/travist/splash_activity.kt
+++ app/src/main/java/com/example/travist/splash_activity.kt
@@ -9,7 +9,7 @@ class splash_activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        val intent = Intent (this,MainActivity::class.java)
+        val intent = Intent (this,LoginActivity::class.java)
 
         Handler().postDelayed({
             startActivity(intent)
