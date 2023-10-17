d95dc0473484ddde8b6d56d7149f9ad54c67d5c5
Khizar Alam <138690124+KhizarAlam20@users.noreply.github.com>
2023-10-17 13:06:05 +0500

    v1.5
    appScreen

--- app/src/main/java/com/example/travist/MainActivity.kt
+++ app/src/main/java/com/example/travist/MainActivity.kt
@@ -3,10 +3,18 @@ package com.example.travist
 //import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.travist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+
+
     }
+
+
 }
