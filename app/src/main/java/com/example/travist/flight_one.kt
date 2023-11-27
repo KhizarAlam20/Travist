e2935d924690c566262d9a894c569f528bf07a32
KhizarAlam20 <khizar.alam20@gmail.com>
2023-11-27 13:10:59 +0500

    rcv update

---
+++ app/src/main/java/com/example/travist/flight_one.kt
@@ -0,0 +1,11 @@
+package com.example.travist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class flight_one : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flight_one)
+    }
+}
